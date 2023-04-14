0c167a65ac8a404f11f0edc1099b85ca3e184106
보배 정 <acarid27@gmail.com>
2023-04-14 19:46:44 +0900

    feat: Add Solution_1
    첫번째 솔루션 구현

--- LevelTest_1/Program.cs
+++ LevelTest_1/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace LevelTest_1
+﻿using LevelTest_1.Solutions;
+
+namespace LevelTest_1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // 1. 문자열 속에 문자 찾기
+            Solution_1 solution_1 = new Solution_1();
+            Console.WriteLine(solution_1.Solution("pineapple is yummy", "apple"));
+            
+
         }
     }
 }
---
+++ LevelTest_1/Solutions/Solution_1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LevelTest_1.Solutions
+{
+    internal class Solution_1
+    {
+        // 문자열 속에 문자 찾기
+        public int Solution(String source, String target)
+        {
+            return source.IndexOf(target);
+        }
+    }
+}
