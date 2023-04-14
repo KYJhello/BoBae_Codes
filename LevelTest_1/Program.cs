ae14fe98cfd0d9021c992751f15b705f56ea4cb0
보배 정 <acarid27@gmail.com>
2023-04-14 21:02:01 +0900

    feat: Add SlideGame
    슬라이드 퍼즐 게임을 실행할 수 있는 슬라이드게임 클래스 구현

--- LevelTest_1/Program.cs
+++ LevelTest_1/Program.cs
@@ -39,6 +39,15 @@ namespace LevelTest_1
             */
             UpDownGame.UpDownGame.GameStart();
 
+            /* 슬라이드 퍼즐 만들기
+            * 1. 5x5 판을 생성하고 랜덤한 숫자를 배치한다.
+            * 2. 시작위치는 상관없으며 ArrowKey입력시 해당 방향으로 이동한다.
+            * 3. 단, 밖으로 벗어날 수 없다.
+            * 4. 아래 예시는 0이 움직이는 것으로 가정한다.
+            */
+            SlideGame.SlideGame.GameStart();
+
+
 
         }
     }
---
+++ LevelTest_1/SlideGame/SlideGame.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LevelTest_1.SlideGame
+{
+    internal class SlideGame
+    {
+        class Point
+        {
+            public int x;
+            public int y;
+        }
+        enum Direction
+        {
+            Up, Down, Left, Right, None
+        }
+        static Direction GameInput()
+        {
+            ConsoleKeyInfo info = Console.ReadKey();    // 키로 입력받아 info에 저장
+            switch (info.Key)
+            {
+                case ConsoleKey.UpArrow:
+                    return Direction.Up;
+                case ConsoleKey.DownArrow:
+                    return Direction.Down;
+                case ConsoleKey.LeftArrow:
+                    return Direction.Left;
+                case ConsoleKey.RightArrow:
+                    return Direction.Right;
+                default:
+                    return Direction.None;
+            }
+        }
+
+        static void CreateBoard(List<List<int>> board, Point point)
+        {
+            Random rand = new Random();
+
+            board.Clear();
+            List<int> selected = new List<int>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                board.Add(new List<int>());
+                for (int j = 0; j < 5; j++)
+                {
+                    int temp;
+                    do
+                    {
+                        temp = rand.Next(0, 25);
+                    } while (selected.Contains(temp));
+                    selected.Add(temp);
+                    board[i].Add(temp);
+                    if (temp == 0)
+                    {
+                        point.x = j;
+                        point.y = i;
+                    }
+                }
+            }
+        }
+
+        static void GameRender(List<List<int>> board)
+        {
+            Console.Clear();
+            for (int i = 0; i < board.Count; i++)
+            {
+                for (int j = 0; j < board[i].Count; j++)
+                {
+                    Console.Write("{0}\t", board[i][j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("\n ← : 왼쪽\t → : 오른쪽\t↑ : 위쪽\t↓ : 아래쪽");
+        }
+        static void GameUpdate(Direction input, Point point, List<List<int>> board)
+        {
+            Point prevPoint = new Point();
+            prevPoint.x = point.x;
+            prevPoint.y = point.y;
+
+            switch (input)
+            {
+                case Direction.Up:
+                    point.y--;
+                    break;
+                case Direction.Down:
+                    point.y++;
+                    break;
+                case Direction.Left:
+                    point.x--;
+                    break;
+                case Direction.Right:
+                    point.x++;
+                    break;
+                default:
+                    break;
+            }
+            if (point.x >= 0 && point.y >= 0 && point.x < 5 && point.y < 5)
+            {
+                board[prevPoint.y][prevPoint.x] = board[point.y][point.x];
+                board[point.y][point.x] = 0;
+            }
+            else
+            {
+                point.x = prevPoint.x;
+                point.y = prevPoint.y;
+            }
+        }
+        public static void GameStart()
+        {
+            Console.Title = "Slide_Puzzle";
+            Console.CursorVisible = false;
+
+            List<List<int>> board = new List<List<int>>();
+            Point point = new Point();
+
+            CreateBoard(board, point);
+            GameRender(board);
+
+            //loop
+            while (true)
+            {
+                //input
+                Direction input = GameInput();
+                //update
+                GameUpdate(input, point, board);
+                //render
+                GameRender(board);
+                //quitCheck
+            }
+
+            //release
+        }
+
+    }
+}
